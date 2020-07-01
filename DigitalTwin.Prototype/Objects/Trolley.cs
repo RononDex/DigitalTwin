490cac9d13f6cfe65e11cfefad01771a79ddba9e
Timo Sbrzesny <timo.sbrzesny@gmail.com>
2020-07-01 13:34:51 +0200

    add trolley for employee

---
+++ DigitalTwin.Prototype/Objects/Trolley.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Simulation;
+
+namespace DigitalTwin.Prototype.Objects
+{
+    public class Trolley : SimulationObject
+    {
+        public IList<ItemProductStatic>? ItemProductStatics
+        {
+            get => (List<ItemProductStatic>?)this[nameof(ItemProductStatics)];
+            set => this[nameof(ItemProductStatics)] = value;
+        }
+
+        public Employee Employee
+        {
+            get => (Employee) this[nameof(Employee)];
+            set => this[nameof(Employee)] = value;
+        }
+    }
+}
