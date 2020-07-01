43f4487a54f540b5dfc25e24433b07fae96b3ae4
RononDex <tinoheuberger@protonmail.com>
2020-07-01 09:48:31 +0200

    Add simulation Framework

---
+++ Simulation.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("3c290a75-d35e-45c5-8e92-74fddfbf25a5")]
---
+++ Simulation.Core/SimulationContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Simulation
+{
+    /// <summary>
+    /// Used by simulation engines
+    /// </summary>
+    public class SimulationContext
+    {
+        public SimulationWorld World { get; private set; }
+
+        /// <summary>
+        /// Creates a new simulation context
+        /// </summary>
+        /// <param name="world">The world of the context</param>
+        public SimulationContext(SimulationWorld world)
+        {
+            this.World = world;
+        }
+    }
+}
