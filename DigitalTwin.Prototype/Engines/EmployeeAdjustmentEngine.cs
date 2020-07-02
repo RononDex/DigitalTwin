04330bd5c531d1d17c8ed5a28dc48f7467dc96e6
Timo Sbrzesny <timo.sbrzesny@gmail.com>
2020-07-02 10:15:28 +0200

    remove employees when there are no new picking tours

--- DigitalTwin.Prototype/Engines/EmployeeAdjustmentEngine.cs
+++ DigitalTwin.Prototype/Engines/EmployeeAdjustmentEngine.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Numerics;
 using DigitalTwin.Prototype.Objects;
+using Microsoft.VisualBasic;
 using Simulation;
 
 namespace DigitalTwin.Prototype.Engines
@@ -10,9 +11,10 @@ namespace DigitalTwin.Prototype.Engines
     {
         public override void UpdateWorld(SimulationContext context, TimeSpan step)
         {
-            var random = new Random((int)step.Ticks);
+            var random = new Random((int) step.Ticks);
             var warehouse = context.World.Objects.First() as Warehouse;
-            if (warehouse.PickingTours.Count(p => p.State == PickingTour.PickingTourState.New) > 2)
+            if (warehouse.PickingTours.Count(p => p.State == PickingTour.PickingTourState.New) >
+                warehouse.Employees.Count)
             {
                 warehouse.Objects.Add(
                     new Employee
@@ -24,14 +26,16 @@ namespace DigitalTwin.Prototype.Engines
                 );
             }
 
-            //while (warehouse.PickingTours.Count < warehouse.Employees.Count(e => e.PickingTour == null) && warehouse.Employees.Count < 2)
-            //{
-            //    var employeeToRemove = warehouse.Employees.FirstOrDefault(e => e.PickingTour == null);
-            //    if (employeeToRemove != null)
-            //    {
-            //        warehouse.Employees.Remove(employeeToRemove);
-            //    }
-            //}
+            while (warehouse.Employees.Count > 2 &&
+                   warehouse.PickingTours.Count(p => p.State == PickingTour.PickingTourState.New) == 0 &&
+                   warehouse.Employees.Count(e => e.PickingTour == null) > 0)
+            {
+                var employeeToRemove = warehouse.Employees.FirstOrDefault(e => e.PickingTour == null);
+                if (employeeToRemove != null)
+                {
+                    warehouse.Objects.Remove(employeeToRemove);
+                }
+            }
         }
     }
 }
